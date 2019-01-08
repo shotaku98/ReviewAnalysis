7c9cde7540384d3d43eeaae8918a0db41b53f327
Shourya Banerjee <shourya98banerjee@gmail.com>
2019-01-08 15:33:26 +0530

    Update SentimentAnalysis.py

---
+++ SentimentAnalysis.py
@@ -0,0 +1,22 @@
+import re
+import tweepy
+from textblob import TextBlob
+
+class Review(object):
+    def __init(self):
+
+    def clean_review(self,review):
+
+    def get_sentiment(self,)
+         """
+        if analysis.sentiment.polarity > 0: 
+            return 'positive'
+        elif analysis.sentiment.polarity == 0: 
+            return 'neutral'
+        else: 
+            return 'negative'
+        """
+
+def main():
+    
+    
