4c2a9988d07bb9421794ad4bc13e0ce5f32b1e2f
Shourya Banerjee <shourya98banerjee@gmail.com>
2019-01-09 21:19:33 +0530

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,29 +1,35 @@
 import re
-import tweepy
 from textblob import TextBlob
+import tweepy
 
 class Review(object):
     def __init(self):
+        print("Object Created")
 
 
-    REPLACE_NO_SPACE = re.compile("(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
-    REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+    
     
     def clean_review(self,reviews):
-         reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
-         reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
-         reviews.split(".")
+        REPLACE_NO_SPACE = re.compile("(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
+        REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+        reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
+        reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
+        reviews.split(".")
 
-    def get_sentiment(self,)
-         """
+    def get_sentiment(self,reviews):
+         
+
+        analysis = TextBlob(self.clean_review(reviews))
+        
         if analysis.sentiment.polarity > 0: 
             return 'positive'
         elif analysis.sentiment.polarity == 0: 
             return 'neutral'
         else: 
             return 'negative'
-        """
+        
 
 def main():
-    
+    api=Review()
+    print(api.get_sentiment("Good food"))
     
